194938c69092b0b1ba5ae7b6e24dfe83b4255bb3
Alejandro Relloso <arelloso@opendeusto.es>
2018-04-14 20:24:22 +0200

    More changes

--- src/main/java/bspq18_e4/GestionHotelera/server/dao/HotelDAO.java
+++ src/main/java/bspq18_e4/GestionHotelera/server/dao/HotelDAO.java
@@ -1,35 +1,35 @@
 package main.java.bspq18_e4.GestionHotelera.server.dao;
 
+import java.util.ArrayList;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Transaction;
+import javax.jdo.Extent;
 
+import main.java.bspq18_e4.GestionHotelera.server.data.Hotel;
+import main.java.bspq18_e4.GestionHotelera.server.data.Reservation;
 import main.java.bspq18_e4.GestionHotelera.server.data.User;
 
-public class HotelDAO implements IHotelDAO{
+public class HotelDAO implements IHotelDAO {
 
 	private PersistenceManagerFactory pmf;
-	
+
 	public HotelDAO() {
 		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
 	}
 
 	public void store(Object objeto) {
-		// Persistence Manager
 		PersistenceManager pm = pmf.getPersistenceManager();
-		// Transaccion para agrupar las operaciones con la bd
 		Transaction tx = pm.currentTransaction();
 
 		try {
-			// Empezamos la transaccion
 			tx.begin();
-			System.out.println("Introduciendo el contenido en la base de datos...");
 			pm.makePersistent(objeto);
-			// Fin de la transccion
 			tx.commit();
 		} catch (Exception ex) {
-			System.out.println("# Error al almacenar los objetos: " + ex.getMessage());
+			System.out.println("# Error storing: " + ex.getMessage());
 		} finally {
 			if (tx != null && tx.isActive()) {
 				tx.rollback();
@@ -41,7 +41,35 @@ public class HotelDAO implements IHotelDAO{
 
 	public void register(User user) {
 		store(user);
-		
+	}
+
+	public void book(Reservation reservation) {
+		store(reservation);
 	}
 	
+	public ArrayList<Hotel> getHotels() {
+		ArrayList<Hotel> hotels = new ArrayList<>();
+		PersistenceManager pm = pmf.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(3);
+		Transaction tx = pm.currentTransaction();
+
+		try {
+			tx.begin();
+			Extent<Hotel> ext = pm.getExtent(Hotel.class, true);
+			for(Hotel hotel : ext){
+				hotels.add(hotel);
+			}
+			tx.commit();
+		} catch (Exception ex) {
+			System.out.println("# Error storing: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		
+		return hotels;
+	}
+
 }
--- src/main/java/bspq18_e4/GestionHotelera/server/remote/Service.java
+++ src/main/java/bspq18_e4/GestionHotelera/server/remote/Service.java
@@ -3,6 +3,7 @@ package main.java.bspq18_e4.GestionHotelera.server.remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import main.java.bspq18_e4.GestionHotelera.server.assembler.Assemble;
 import main.java.bspq18_e4.GestionHotelera.server.dao.HotelDAO;
 import main.java.bspq18_e4.GestionHotelera.server.dao.IHotelDAO;
 
@@ -10,9 +11,16 @@ public class Service extends UnicastRemoteObject implements IService {
 
 	private static final long serialVersionUID = 1L;
 	private IHotelDAO dao;
+	private Assemble ass;
 
 	public Service() throws RemoteException {
 		dao = new HotelDAO();
+		ass = new Assemble();
 	}
+	
+//	public UserDTO SignIn(String email, String pass) throws RemoteException{
+//		User
+//	}
+//	
 
 }
